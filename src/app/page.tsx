a81666d5ec7c2fcf871616c9496538687fb4de8e
PowerzinBR <luizotavionunesfelix@gmail.com>
2024-10-09 13:07:19 -0300

    3: customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-      Hello World
+    <div className="">
+      <Button variant="primary" size="xs">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="destructive">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="muted">Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="teritary">Click me</Button>
+      <Input placeholder="Test input" />
     </div>
   );
 }
