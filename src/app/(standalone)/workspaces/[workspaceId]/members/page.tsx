43a1cdcf2370df36f02687ee5b71be9d80a862fa
PowerzinBR <luizotavionunesfelix@gmail.com>
2024-11-16 14:05:32 -0300

    23: workspace members

---
+++ src/app/(standalone)/workspaces/[workspaceId]/members/page.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/queries";
+import { MembersList } from "@/features/members/components/members-list";
+
+const MembersPage = async () => {
+	const current = await getCurrent();
+	if (!current) redirect("/sign-in");
+	
+  return (
+		<div className="w-full lg:max-w-xl">
+			<MembersList />
+		</div>
+	);
+};
+
+export default MembersPage;
